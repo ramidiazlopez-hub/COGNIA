a39601330855c8a6ffc7ac1f821a4c4abc4683b7
ramidiazlopez-hub <ramidiazlopez@gmail.com>
2026-04-28 20:13:16 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://ivmhnutmzcperjxlvfat.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2bWhudXRtemNwZXJqeGx2ZmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNDM4MjQsImV4cCI6MjA5MjgxOTgyNH0.3EvkUW-ZPWLjqKCPuYUUTEH95wV47bqU6pIUeHznmc0'
-
+ 
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+ 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
